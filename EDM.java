ba299b0957170f406ff135c696d167db59d363f4
Lev <lev.petryakov@gmail.com>
2021-11-10 14:47:57 +0200

    Code refactor Add new consts for better orientation

--- EDM.java
+++ EDM.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class EDM {
     private static Scanner scanner;
+    private final static int delay = 1;
+    private final static int delay_pause = 1;
+    private final static int[] blank_title = new int[] {3, 2};
+    private final static int[] blank_heading = new int[] {2, 1}; 
 
     private static Participant[] testParticipants = new Participant[]{
         new Participant("Ave", Race.Elf),
@@ -36,12 +40,12 @@ public class EDM {
 
     public EDM(){
         scanner = new Scanner(System.in);
-        animatedOutputWithBlanks("--- Welcome to Elves & Dwarves Misfits ---", 20, 1, 3, 2);
+        animatedOutputWithBlanks("--- Welcome to Elves & Dwarves Misfits ---", blank_title[0], blank_title[1]);
     }
 
     public int setup(){
-        animatedOutput("Choose between automated control (1) or manual control (2) ", 20, 1, true);
-        animatedOutput("Control type : ", 20, 1, false);
+        animatedOutput("Choose between automated control (1) or manual control (2) ", true);
+        animatedOutput("Control type : ", false);
         String input = scanner.nextLine();
         if (input.equals("1") || input.equals("2"))
             return Integer.parseInt(input);
@@ -55,7 +59,7 @@ public class EDM {
         } else if (contolType == 2) {
 
         } else {
-            animatedOutputWithBlanks("Something went wrong ! Please start from the beginning. ", 20, 1, 2, 1);
+            animatedOutputWithBlanks("Something went wrong ! Please start from the beginning. ", blank_heading[0], blank_heading[1]);
             start(setup());
         }
     }
@@ -66,7 +70,7 @@ public class EDM {
     }
 
     public void outputParticipants(List<Participant> participants){
-        animatedOutputWithBlanks("- Participants -", 20, 1, 2, 1);
+        animatedOutputWithBlanks("- Participants -", blank_heading[0], blank_heading[1]);
             String elves = "";
             String dwarves = "";
             for (Participant participant : participants) {
@@ -77,12 +81,12 @@ public class EDM {
                     dwarves += (!dwarves.isEmpty()) ? ", " + participant.getName() : participant.getName();
                 }
             }
-            animatedOutput("Elves: "+elves, 20, 10, true);
-            animatedOutput("Dwarves: "+dwarves, 20, 10, true);
+            animatedOutput("Elves: "+elves, true);
+            animatedOutput("Dwarves: "+dwarves, true);
     }
 
     public void outputEncounters(List<Encounter> encounters){
-        animatedOutputWithBlanks("- Encounters -", 20, 1, 2, 1);
+        animatedOutputWithBlanks("- Encounters -", blank_heading[0], blank_heading[1]);
             for (Encounter encounter : encounters) {
                 Participant elf = encounter.getElf();
                 Swing elvenSwing = encounter.getElfinSwing();
@@ -97,17 +101,17 @@ public class EDM {
                                 " x " + 
                                 dwarvenSwing.getSwingCount() + 
                                 dwarvenSwing.getSwingType();
-                animatedOutput(output, 20, 10, true);
+                animatedOutput(output, true);
             }
     }
 
-    public void animatedOutput(String output, int delay, int delayMultiply, boolean end){
+    public void animatedOutput(String output, boolean end){
         char[] outputChars = output.toCharArray();
         for (char c : outputChars) {
             System.out.print(c);
             try {
                 if (c == ',' || c == '.'){
-                    Thread.sleep(delay*delayMultiply);
+                    Thread.sleep(delay_pause);
                 } else {
                     Thread.sleep(delay);
                 }
@@ -119,7 +123,7 @@ public class EDM {
             System.out.println();
     }
 
-    public void animatedOutputWithBlanks(String output, int delay, int delayMultiply, int beforeBlankCount, int afterBlankCount){
+    public void animatedOutputWithBlanks(String output, int beforeBlankCount, int afterBlankCount){
         for (int i = 0; i < beforeBlankCount; i++) {
             System.out.println();
         }
@@ -128,7 +132,7 @@ public class EDM {
             System.out.print(c);
             try {
                 if (c == ',' || c == '.'){
-                    Thread.sleep(delay*5);
+                    Thread.sleep(delay_pause);
                 } else {
                     Thread.sleep(delay);
                 }
